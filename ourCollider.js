76ce4ecd12aaca1c1476f389b195798873e6ef00
Hack Reactor Students <github@hackreactor.com>
2013-10-05 13:22:17 -0700

    Our guys move!

--- ourCollider.js
+++ ourCollider.js
@@ -1,3 +1,6 @@
+(function() {
+  var axes, enemyData,gameBoard, gameOptions,gameStats,play,render;
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   gameOptions = {
     height: 450,
@@ -19,70 +22,78 @@
   gameBoard = d3.select('.container').append('svg:svg').attr('width', gameOptions.width).attr('height', gameOptions.height);
 
 
+
+  // var enemyData = _.range(0,gameOptions.nEnemies).map(function(i){
+  //   return {id: i, x:Math.random()*100,y:Math.random()*100};
+  // });
+  // var listOfEnemies = d3.selectAll('circle').data(enemyData, function(d) {return d.id;});
+  // svgElement.selectAll('circle').data(enemyData).enter().append('circle');
+  // svgElement.selectAll('circle').attr("cx",function(enemyData){return axes.x(enemyData.x);})
+  //   .attr("cy",function(enemyData){return axes.y(enemyData.y);}).attr("r",10);
+
+  enemyData = function() {
+    return _.range(0,gameOptions.nEnemies).map(function(i){
+      return {
+        id: i,
+        x:Math.random()*100,
+        y:Math.random()*100
+      };
+    });
+  };
+
+
+  render = function(enemy_data) {
+  var enemies,tweenWithoutCollision;
+
+  enemies = gameBoard.selectAll('circle').data(enemy_data, function(d) {return d.id;});
+
   var svgElement = d3.select('.container').select('svg');
-  var enemyData = _.range(0,gameOptions.nEnemies).map(function(i){
-    return {id: i, x:Math.random()*100,y:Math.random()*100};
-  });
-  var listOfEnemies = d3.selectAll('circle').data(enemyData, function(d) {return d.id;});
-
-
-
-  svgElement.selectAll('circle').data(enemyData).enter().append('circle');
-  svgElement.selectAll('circle').attr("cx",function(enemyData){return axes.x(enemyData.x);})
+  enemies.enter().append('circle').attr("cx",function(enemyData){return axes.x(enemyData.x);})
     .attr("cy",function(enemyData){return axes.y(enemyData.y);}).attr("r",10);
 
-  listOfEnemies.transition().duration(500);
+  tweenWithoutCollision = function(endData) {
+    var endPos, enemy, startPos;
+    enemy = d3.select(this);
+    startPos = {
+      x: parseFloat(enemy.attr('cx')),
+      y: parseFloat(enemy.attr('cy'))
+    };
+    endPos = {
+      x: axes.x(endData.x),
+      y: axes.y(endData.y)
+    };
+    return function(t) {
+      var enemyNextPos;
+//      checkCollision(enemy, onCollision);
+      enemyNextPos = {
+        x: startPos.x + (endPos.x - startPos.x) * t,
+        y: startPos.y + (endPos.y - startPos.y) * t
+      };
+      return enemy.attr('cx', enemyNextPos.x).attr('cy', enemyNextPos.y);
+    };
+  };
+  return enemies.transition().duration(2000).tween('custom', tweenWithoutCollision);
+};
 
-
-  // return enemies.transition().duration(500).attr('r', 10).transition().duration(2000).tween('custom', tweenWithCollisionDetection);
-
-
-  // render = function(enemy_data) {
-  //   // var checkCollision, enemies, onCollision, tweenWithCollisionDetection;
-  //   // enemies = gameBoard.selectAll('circle.enemy').data(enemy_data, function(d) {
-  //   //   return d.id;
-  //   // });
-  //   // enemies.enter().append('svg:circle').attr('class', 'enemy').attr('cx', function(enemy) {
-  //   //   return axes.x(enemy.x);
-  //   // }).attr('cy', function(enemy) {
-  //   //   return axes.y(enemy.y);
-  //   // }).attr('r', 0);
-  //   // enemies.exit().remove();
-  //   checkCollision = function(enemy, collidedCallback) {
-  //     return _(players).each(function(player) {
-  //       var radiusSum, separation, xDiff, yDiff;
-  //       radiusSum = parseFloat(enemy.attr('r')) + player.r;
-  //       xDiff = parseFloat(enemy.attr('cx')) - player.x;
-  //       yDiff = parseFloat(enemy.attr('cy')) - player.y;
-  //       separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-  //       if (separation < radiusSum) return collidedCallback(player, enemy);
-  //     });
-  //   };
-  //   onCollision = function() {
-  //     updateBestScore();
-  //     gameStats.score = 0;
-  //     return updateScore();
-  //   };
-  //   tweenWithCollisionDetection = function(endData) {
-  //     var endPos, enemy, startPos;
-  //     enemy = d3.select(this);
-  //     startPos = {
-  //       x: parseFloat(enemy.attr('cx')),
-  //       y: parseFloat(enemy.attr('cy'))
-  //     };
-  //     endPos = {
-  //       x: axes.x(endData.x),
-  //       y: axes.y(endData.y)
-  //     };
-  //     return function(t) {
-  //       var enemyNextPos;
-  //       checkCollision(enemy, onCollision);
-  //       enemyNextPos = {
-  //         x: startPos.x + (endPos.x - startPos.x) * t,
-  //         y: startPos.y + (endPos.y - startPos.y) * t
-  //       };
-  //       return enemy.attr('cx', enemyNextPos.x).attr('cy', enemyNextPos.y);
-  //     };
-  //   };
-  //   return enemies.transition().duration(500).attr('r', 10).transition().duration(2000).tween('custom', tweenWithCollisionDetection);
+  play = function() {
+  var gameTurn;
+  gameTurn = function() {
+    var newEnemyPositions;
+    newEnemyPositions = enemyData();
+    return render(newEnemyPositions);
+  };
+  // increaseScore = function() {
+  //   gameStats.score += 1;
+  //   return updateScore();
   // };
+  gameTurn();
+  setInterval(gameTurn, 2000);
+  //return setInterval(increaseScore, 50);
+};
+
+play();
+
+}).call(this);
+
+
+
